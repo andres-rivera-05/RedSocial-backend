054621f483882a32e98a55436ba89f8a6957c47b
andres <148400031+andres-rivera-05@users.noreply.github.com>
2024-03-16 15:10:15 -0600

    Eliminado

--- routes/apiLibros.js
+++
@@ -1,8 +0,0 @@
-import Express from "express";
-const libro = Express();
-import { getAuth } from "../controllers/controllerLibro.js";
-
-
-libro.get('/auth/:nombre_usuario/:pass', getAuth);
-
-export { libro }
